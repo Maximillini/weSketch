fb5ccd08ffe4b2889351954b98d770e6bbe7dc78
Max Iniguez <maximillini86@gmail.com>
2024-05-30 14:25:45 -0500

    Rearrange GamePage to achieve desired layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div className="app-container">
-      <h1>Welcome to weSketch! {playerHandle}</h1>
+      {/* <h1>Welcome to weSketch! {playerHandle}</h1> */}
       <GamePage />
     </div>
   )
