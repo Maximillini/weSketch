85ee0596a92fb25b5eba4574da7cbbcbc793b052
Max Iniguez <maximillini86@gmail.com>
2024-06-01 10:42:01 -0500

    Add mock data generator helper functions, comment out code in Canvas component

--- client/src/helpers/mockData.ts
+++ client/src/helpers/mockData.ts
@@ -5,6 +5,11 @@ type MockUser = {
   score: number
 }
 
+type MockChat = {
+  userName: string,
+  message: string
+}
+
 export const generateUsers = (count: number) => {
   const users: MockUser[] = []
 
@@ -14,5 +19,17 @@ export const generateUsers = (count: number) => {
     users.push({ userName, score: faker.number.int(10) })
   }
 
+  users.sort((a, b) => b.score - a.score)
+  console.log({ users })
   return users
+}
+
+export const generateChatLog = (playersList: MockUser[], chatsAmount: number): MockChat[] => {
+  const chats = []
+  
+  for (let i = 0; i < chatsAmount; i++) {
+    chats.push({ userName: playersList[Math.floor(Math.random() * playersList.length)].userName, message: faker.lorem.lines({ min: 1, max: 3 }) })
+  }
+
+  return chats
 }
