5dd2a6eca42b9150e10a7d5044d5314fd34b5e7a
Max Iniguez <maximillini86@gmail.com>
2024-06-01 19:06:00 -0500

    Add server/socket code

---
+++ server/index.js
@@ -0,0 +1,74 @@
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ["GET", "POST"]
+  }
+})
+
+const activeUsers = {}
+
+io.on('connection', (socket) => {
+  console.log('a user connected!', socket.id);
+
+  socket.on('draw', (data) => {
+    console.log({ data })
+    socket.broadcast.emit('draw', data);
+  });
+
+  socket.on('clear', () => {
+    socket.broadcast.emit('clear');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    handleLeaveChat(socket, io)
+  });
+
+  socket.on('leave chat', () => handleLeaveChat(socket, io));
+  
+  socket.on('register-user', (handleValue) => {
+    console.log({ handleValue })
+    
+    if (normalizedUsersList().includes(handleValue.toLowerCase())) {
+      return socket.emit('validation error', 'existing');
+    }
+    
+    console.log(`${handleValue} has joined!`);
+    
+    activeUsers[socket.id] = handleValue;
+    console.log({ activeUsers })
+    // io.emit('register user', Object.values(activeUsers));
+    socket.emit('user-added', Object.values(activeUsers))
+  });
+
+  socket.on('chat message', (chatData) => {
+    console.log({ chatData })
+    console.log({ activeUsers })
+    console.log(socket.id)
+
+    if (chatData.chatType === 'general') return io.emit('general message', { userName: chatData.userName, message: chatData.message })
+    
+    console.log('this should be showing for game messages only')
+    
+    io.emit('game message', { userName: chatData.userName, message: chatData.message })
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const normalizedUsersList = () => Object.values(activeUsers).map((user) => user.toLowerCase()) ?? [];
+
+const handleLeaveChat = (socket, io) => {
+  if (!activeUsers[socket.id]) return;
+  console.log(`${activeUsers[socket.id]} disconnected...`);
+  const outboundUser = activeUsers[socket.id]; 
+  delete activeUsers[socket.id];
+  io.emit('leave chat', Object.values(activeUsers), outboundUser);
+}
