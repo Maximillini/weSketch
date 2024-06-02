d1fa4ddb3b05f296f890de23b29502c2ba6924a6
Max Iniguez <maximillini86@gmail.com>
2024-06-01 19:08:17 -0500

    Connect LoginPage component to socket/server to get player list

--- client/src/components/LoginPage/index.tsx
+++ client/src/components/LoginPage/index.tsx
@@ -1,13 +1,24 @@
 import { ChangeEvent, useState } from "react"
 import { usePlayerStore } from "../../stores/playerStore"
+import { io } from 'socket.io-client'
+import { useGameStore } from "../../stores/gameStore"
+
+const socket = io('http://localhost:4000')
+socket.connect()
 
 export const LoginPage = () => {
+  const addPlayer = useGameStore((state) => state.addPlayer)
   const setHandle = usePlayerStore((state) => state.setHandle)
   const [handleValue, setHandleValue] = useState<string>('')
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value)
     setHandleValue(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    setHandle(handleValue)
+    addPlayer({ userName: handleValue, score: 0 })
+    socket.emit('register-user', handleValue)
   }
 
   return (
@@ -15,7 +26,7 @@ export const LoginPage = () => {
       <h1>Login</h1>
       <div className="login-box">
         <input type="text" placeholder="Handle" value={handleValue} onChange={(e) => handleChange(e)}/>
-        <input type="button" value="Submit" onClick={() => { setHandle(handleValue) }}/>
+        <input type="button" value="Submit" onClick={handleSubmit}/>
       </div>
     </div>
   )
