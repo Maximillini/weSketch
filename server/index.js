0782e692d7f300abe89f84ce15bdeb194db4a379
Max Iniguez <maximillini86@gmail.com>
2024-06-03 13:13:24 -0500

    Update imports to include path and fileURLToPath

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import path from 'path'
+import { fileURLToPath } from 'url';
 import http from 'http'
 import { Server } from 'socket.io'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 const server = http.createServer(app)
