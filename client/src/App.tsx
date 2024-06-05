7884b1de5a715d23b067aab8d3f4dfa8cc3f18c6
Max Iniguez <maximillini86@gmail.com>
2024-06-05 15:05:42 -0500

    Add socket store, initialize socket once at App component level

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,20 @@
-import { GamePage } from './components/GamePage/index';
-import { LoginPage } from './components/LoginPage';
-import { usePlayerStore } from './stores/playerStore';
+import { useEffect } from 'react'
+import { GamePage } from './components/GamePage/index'
+import { LoginPage } from './components/LoginPage'
+import { usePlayerStore } from './stores/playerStore'
+import { useSocketStore } from './stores/socketStore'
 import './App.css'
 
 function App() {
   const handle = usePlayerStore((state) => state.handle)
+  const initializeSocket = useSocketStore((state) => state.initializeSocket)
+
+  useEffect(() => {
+    initializeSocket()
+  }, [initializeSocket])
+
   // TODO - Add Routing via React or Tanstack Router
-  return (
-    <>
-      {handle !== '' ? <GamePage /> : <LoginPage />}
-    </>
-  )
+  return <>{handle !== '' ? <GamePage /> : <LoginPage />}</>
 }
 
 export default App
