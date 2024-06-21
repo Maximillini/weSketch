1f291fcec2edede307216a02582f09802285fc2c
Max Iniguez <maximillini86@gmail.com>
2024-06-21 14:14:47 -0500

    Pull Color and Tool enums out into separate type library file

---
+++ client/src/types/canvasTypes.ts
@@ -0,0 +1,19 @@
+export enum Colors {
+  Black = 'black',
+  Blue = 'blue',
+  Green = 'green',
+  Orange = 'orange',
+  Pink = 'pink',
+  Purple = 'purple',
+  Red = 'red',
+  Yellow = 'yellow',
+  Turquoise = 'turquoise',
+  White = 'white'
+}
+
+export enum Tools {
+  Pencil = 'edit',
+  Line = 'pen_size_1',
+  Bucket = 'colors',
+  Eraser = 'ink_eraser'
+}
