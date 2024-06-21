028ad3ec76bf6ece4357939acc62c3970baf5182
Max Iniguez <maximillini86@gmail.com>
2024-06-21 14:15:47 -0500

    Update useGameStore with new gameState object, functions

--- client/src/stores/gameStore.ts
+++ client/src/stores/gameStore.ts
@@ -1,18 +1,34 @@
 import { create } from 'zustand'
-import { Player, Chat, ChatType } from '../types/gameTypes'
+import { Player, Chat, ChatType } from '../types/gameTypes';
 
 type GameStoreState = {
   playerList: Player[],
   gameChatLog: { userName: string, message: string }[],
   generalChatLog: { userName: string, message: string }[],
+  gameState: {
+    hasStarted: boolean,
+    currentRound: number,
+    currentWordList: string[],
+    currentArtist: Player | null
+  },
+  setPlayers: (playerList: Player[]) => void
+  startGame: () => void
   addPlayer: (newPlayer: Player) => void,
-  addChat: (newChat: Chat, chatType: ChatType) => void
+  addChat: (newChat: Chat, chatType: ChatType) => void,
 }
 
 export const useGameStore = create<GameStoreState>((set) => ({
   playerList: [],
   gameChatLog: [],
   generalChatLog: [],
+  gameState: {
+    hasStarted: false,
+    currentRound: 1,
+    currentWordList: ['', '', ''],
+    currentArtist: null,
+  },
+  setPlayers: (playerList) => set({ playerList }),
+  startGame: () => set(<Partial<GameStoreState>>({ gameState: { hasStarted: true }})),
   addPlayer: (newPlayer) => set((state) => ({ playerList: [...state.playerList, newPlayer] })),
   addChat: (newChat, chatType) => set((state) => ({ [chatType]: [...state[chatType], newChat]}))
 }))
